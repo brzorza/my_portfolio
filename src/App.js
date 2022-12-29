913ef1c167116cf7cc8c90fdce3fb13f9c0fd327
Wiktor Brzoza <wiktorbrzoza13@gmail.com>
2022-12-29 21:22:22 +0100

    Forgot to stage those

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './css/main.min.css';
 import './css/styles.css';
+import './css/main.min.css';
+import '../node_modules/bootstrap/js/index.esm';
 import Navbar from './components/Navbar';
 
 function App() {
