c0c349d5dd32cb958ace9fc9d13b0fbe58ad2ad7
Wiktor Brzoza <wiktorbrzoza13@gmail.com>
2023-01-03 21:35:36 +0100

    Adding image and styling About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import me from '../img/me.jpg'
-
 export default function About() {
     return(
-        <div className="text-white" style={{background: "var(--background2)"}}>
-            <div className="container">
-                <div className="text-center row justify-content-center">
-                    <div className="row col-sm-6">
-                        <img src={me} alt="Wiktor Brzoza"  />
+        <div className="text-white pt-5" style={{background: "var(--background2)"}}>
+            <div className="container pb-5">
+                <div className="text-center row gap-5">
+                    <div className="row col-lg">
+                        <div className='text-center px-2 px-lg-5'>
+                            <img 
+                                src={require('../img/me.png')} 
+                                className='img-fluid about-image'
+                                alt="Wiktor Brzoza"
+                            />
+                        </div>
                     </div>
-                    <div className="row col-sm-6">
-                        <p>loremloremloremloremloremloremloremloremlorem</p>
+                    <div className="row col-lg">
+                        <h1>Who am I?</h1>
                     </div>
                 </div>
             </div>
