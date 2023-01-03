39b7be7117586e9e39d453f620296ad6410b8ff6
Wiktor Brzoza <wiktorbrzoza13@gmail.com>
2023-01-03 20:42:15 +0100

    Adding photo

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import me from '../img/me.jpg'
+
 export default function About() {
     return(
-        <div className="container">
-            <div className="row">
-                <div className="col">1</div>
-                <div className="col">2</div>
+        <div className="text-white" style={{background: "var(--background2)"}}>
+            <div className="container">
+                <div className="text-center row justify-content-center">
+                    <div className="row col-sm-6">
+                        <img src={me} alt="Wiktor Brzoza"  />
+                    </div>
+                    <div className="row col-sm-6">
+                        <p>loremloremloremloremloremloremloremloremlorem</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
